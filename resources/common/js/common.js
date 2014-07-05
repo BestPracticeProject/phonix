cb374752899c8b960de9c1fcdf9a9412f851a434
dima <dspwww@gmail.com>
2014-07-06 01:01:19 +0400

    init commit

---
+++ resources/common/js/common.js
@@ -0,0 +1,24 @@
+;(function($){
+	'use strict';
+
+	/* trigger when page is ready */
+	$(function(){
+
+			$('.j-slider_info').bxSlider({
+				mode: 'fade',
+				captions: true,
+				controls: false
+			});
+
+	});
+
+	/* optional triggers
+	$(window).load(function() {
+
+	});
+
+	$(window).resize(function() {
+
+	});
+	*/
+}(window.jQuery));
